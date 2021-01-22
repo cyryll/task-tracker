df0423df6c2eb6da3945a9987a58f18a92a8264d
cyryll <cyrylpaull@gmail.com>
2021-01-22 10:29:35 +0300

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   // const name = 'paul'
   // const x = true
+  const [showAddTask, setShowAddTask] = useState (false)
+
   const [tasks, setTasks] = useState(
     [
-        {
+         {
             id: 1,
             text: 'Doctors Appointments',
             day: 'Feb 5th at 1:20pm',
@@ -28,6 +31,14 @@ function App() {
     ]
 )
 
+//add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -43,7 +54,15 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
+      <Header 
+      onAdd = {() => setShowAddTask(!showAddTask)}
+      showAdd= {showAddTask}
+      />
+
+
+      {/*show add task if shoAddTask state is true */}
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+
       {tasks.length >0 ? (
       <Tasks tasks = {tasks} onDelete = {deleteTask} 
       onToggle = {toggleReminder}/>)
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+    
     return (
         <header className='header'>
             {/* <h1 style = {{color:'purple', backgroundColor: 'black'}}>{props.value2}</h1> */}
             {/* <h1 style = {headingStyle}>{props.value2}</h1> */}
             <h1>{props.value2}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button 
+            color={props.showAdd ? 'red' : 'green'} 
+            text={props.showAdd ? 'Close' : 'Add'} 
+            onClick={props.onAdd}/>
         </header>
     )
 }
